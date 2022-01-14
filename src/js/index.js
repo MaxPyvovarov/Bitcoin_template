1339a27229930ea402e1fe60186e11f39465612f
Head4oT <max.pivovar@gmail.com>
2022-01-14 21:41:30 +0200

    Header responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 window.$ = window.jQuery = require('jquery');
+
+//Mobile menu
+$('.menu-icon').on('click', function() {
+    $('.menu').addClass('active');
+})
+$('.cancel-icon').on('click', function() {
+    $('.menu').removeClass('active');
+});
