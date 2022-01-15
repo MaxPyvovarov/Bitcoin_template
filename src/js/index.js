75eca341ccaf5bf049a423d31ab12acd1499e9c0
Head4oT <max.pivovar@gmail.com>
2022-01-15 13:03:36 +0200

    Promo and charts

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,18 @@
 window.$ = window.jQuery = require('jquery');
 
-//Mobile menu
-$('.menu-icon').on('click', function() {
-    $('.menu').addClass('active');
-})
-$('.cancel-icon').on('click', function() {
-    $('.menu').removeClass('active');
+$(document).ready(function () {
+    //Mobile menu
+    $('.menu-icon').on('click', function() {
+        $('.menu').addClass('active');
+    })
+    $('.cancel-icon').on('click', function() {
+        $('.menu').removeClass('active');
+    });
+
+    //Tabs
+    $('.tabs__button').on('click', function () {
+        $(".tabs .tabs__button").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tabs__item").hide().eq($(this).index()).fadeIn();
+    }).eq(0).addClass("active");
+    $(".tabs__item").eq(0).fadeIn();
 });
