7cc0fb0e50c44c8e225f942419026c2a2da558e4
Head4oT <max.pivovar@gmail.com>
2022-01-17 21:38:51 +0200

    Marquee and video section

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 window.$ = window.jQuery = require('jquery');
+import  'jquery.marquee';
 import Swiper, { Autoplay, Navigation } from 'swiper';
 Swiper.use([ Autoplay, Navigation]);
 
@@ -11,10 +12,31 @@ $(document).ready(function () {
         $('.menu').removeClass('active');
     });
 
+    const marqueeInitialization = () => {
+        $('.tabs__marquee').marquee({
+            duration: 8000,
+            pauseOnHover: false,
+            gap: 30,
+          });
+    }
+
     //Tabs
     $('.tabs__button').on('click', function () {
         $(".tabs .tabs__button").removeClass("active").eq($(this).index()).addClass("active");
         $(".tabs__item").hide().eq($(this).index()).fadeIn();
+        $('.tabs__marquee').marquee('destroy');
+        marqueeInitialization();
     }).eq(0).addClass("active");
     $(".tabs__item").eq(0).fadeIn();
+
+    marqueeInitialization();
+
+    const videoPlayer = $('#video');
+
+    function playPause() {
+        if(videoPlayer.paused)
+        videoPlayer.play();
+        else 
+            videoPlayer.pause();
+    }
 });
