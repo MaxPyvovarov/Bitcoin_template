4200a1a0d68594c95a672d07e2138cca82b7566f
MaxPyvovarov <max.pivovar@gmail.com>
2022-08-13 21:03:27 +0300

    jQuery removed and other minor improvements

--- src/js/index.js
+++ src/js/index.js
@@ -1,100 +1,145 @@
 window.$ = window.jQuery = require('jquery');
-import  'jquery.marquee';
-import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
-Swiper.use([ Autoplay, Navigation, Pagination]);
+import 'jquery.marquee';
+import Swiper, {Autoplay, Navigation, Pagination} from 'swiper';
+Swiper.use([Autoplay, Navigation, Pagination]);
 
-$(document).ready(function () {
-    //Search
-    let search = document.querySelector(".search")
-    let btn = document.querySelector(".search-button")
-    let input = document.querySelector(".input")
+window.addEventListener('DOMContentLoaded', () => {
+	//Search
+	const search = document.querySelector('.search'),
+		searchBtn = document.querySelector('.search-button'),
+		input = document.querySelector('.input'),
+		menu = document.querySelector('.menu'),
+		menuBtn = document.querySelector('.menu-icon'),
+		closeMenuBtn = document.querySelector('.cancel-icon');
 
-    btn.addEventListener('click',()=>{
-        search.classList.toggle('active');
-        input.focus()
-    });
-    //Mobile menu
-    $('.menu-icon').on('click', function() {
-        $('.menu').addClass('active');
-    })
-    $('.cancel-icon').on('click', function() {
-        $('.menu').removeClass('active');
-    });
+	searchBtn.addEventListener('click', () => {
+		search.classList.toggle('active');
+		input.focus();
+	});
 
-    //Anchor links
-    $('a').on("click", function (event) {
-        if (this.hash !== '') {
-            event.preventDefault();
-            const hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function () {
-                window.location.hash = hash;
-            });
-        }
-    });
+	//Mobile menu
+	menuBtn.addEventListener('click', () => {
+		menu.classList.add('active');
+	});
 
+	closeMenuBtn.addEventListener('click', () => {
+		menu.classList.remove('active');
+	});
 
-    //Home swiper
-    const home_swiper = new Swiper('.home__swiper', {
-        loop: true,
-        autoplay: {
-            delay: 5000,
-        },
-        slidesPerView: 1,
-        spaceBetween: 30,
-        autoHeight: 'true',
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
+	//Home swiper
+	const home_swiper = new Swiper('.home__swiper', {
+		loop: true,
+		autoplay: {
+			delay: 5000,
+		},
+		slidesPerView: 1,
+		spaceBetween: 30,
+		autoHeight: 'true',
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
 
+	//Testimonial swiper
+	const testimonial_swiper = new Swiper('.testimonial__swiper', {
+		loop: true,
+		autoplay: {
+			delay: 5000,
+		},
+		slidesPerView: 1,
+		spaceBetween: 500,
+		autoHeight: 'true',
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+			autoheight: true,
+		},
+	});
 
-    //Testimonial swiper
-    const testimonial_swiper = new Swiper('.testimonial__swiper', {
-        loop: true,
-        autoplay: {
-            delay: 5000,
-        },
-        slidesPerView: 1,
-        spaceBetween: 500,
-        autoHeight: 'true',
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
-            autoheight: true
-        },
-    });
+	//Tabs
+	// $('.tabs__button')
+	// 	.on('click', function() {
+	// 		$('.tabs .tabs__button')
+	// 			.removeClass('active')
+	// 			.eq($(this).index())
+	// 			.addClass('active');
+	// 		$('.tabs__item')
+	// 			.hide()
+	// 			.eq($(this).index())
+	// 			.fadeIn();
+	// 	})
+	// 	.eq(0)
+	// 	.addClass('active');
+	// $('.tabs__item')
+	// 	.eq(0)
+	// 	.fadeIn();
 
+	//Tabs
+	function tabs(
+		tabsButtonSelector,
+		tabsItemSelector,
+		tabsParentSelector,
+		activeClass
+	) {
+		const tabs = document.querySelectorAll(tabsButtonSelector),
+			tabsItem = document.querySelectorAll(tabsItemSelector),
+			tabsParent = document.querySelector(tabsParentSelector);
 
-    const marqueeInitialization = () => {
-        $('.tabs__marquee').marquee({
-            duration: 8000,
-            pauseOnHover: true,
-            gap: 30,
-          });
-    }
+		function hideTabContent() {
+			tabsItem.forEach((tab) => {
+				tab.classList.remove(activeClass, 'fade');
+			});
 
-    //Tabs
-    $('.tabs__button').on('click', function () {
-        $(".tabs .tabs__button").removeClass("active").eq($(this).index()).addClass("active");
-        $(".tabs__item").hide().eq($(this).index()).fadeIn();
-        $('.tabs__marquee').marquee('destroy');
-        marqueeInitialization();
-    }).eq(0).addClass("active");
-    $(".tabs__item").eq(0).fadeIn();
+			tabs.forEach((tab) => {
+				tab.classList.remove(activeClass);
+			});
+		}
 
-    marqueeInitialization();
+		function showTabContent(i = 0) {
+			tabsItem[i].classList.add(activeClass, 'fade');
+			tabs[i].classList.add(activeClass);
+		}
 
-    $('.video__play').on('click', function (){
-        const videoPlayer = $('#video');
-        $(this).toggleClass('paused');
-        const video = videoPlayer.get(0);
-        if (video.paused)
-            video.play();
-        else
-            video.pause()
-    })
+		hideTabContent();
+		showTabContent();
+
+		tabsParent.addEventListener('click', (event) => {
+			const target = event.target;
+
+			if (target && target.classList.contains(tabsButtonSelector.slice(1))) {
+				tabs.forEach((tab, i) => {
+					if (tab === target && !target.classList.contains(activeClass)) {
+						hideTabContent();
+						showTabContent(i);
+					}
+				});
+			}
+		});
+	}
+	//tabButtonSelector, tabsItemSelector, tabsParentSelector, activeClass
+	tabs('.tabs__button', '.tabs__item', '.tabs__buttons', 'active');
+
+	//Video
+	const videoWrapper = document.querySelector('.video__play'),
+		video = document.querySelector('#video');
+
+	async function playVideo() {
+		try {
+			await video.play();
+			videoWrapper.classList.remove('paused');
+		} catch (err) {
+			videoWrapper.classList.add('paused');
+		}
+	}
+
+	videoWrapper.addEventListener('click', () => {
+		if (videoWrapper.classList.contains('paused')) {
+			playVideo();
+		} else {
+			video.pause();
+			videoWrapper.classList.add('paused');
+		}
+	});
 });
